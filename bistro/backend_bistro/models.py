38352ee879548e52873d93d5600b1d3acc0bc91f
Anansi86 <shawn.staton@student.awesomeinc.org>
2022-11-11 12:34:14 +0000

    changed request

--- bistro/backend_bistro/models.py
+++ bistro/backend_bistro/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class menu_item(models.Model):
-    title = models.CharField(max_length=200, null=False, blank=False, unique=True)
+    title = models.CharField(max_length=30, null=False, blank=False, unique=True)
     description = models.CharField(max_length=200, null=False, blank=False, unique=True)
     price = models.FloatField()
     spicey_level = models.IntegerField()
@@ -13,14 +13,16 @@ class menu_item(models.Model):
         return self.title
 
 class category(models.Model):
-    title = models.CharField(max_length=200, null=True, default=None)
+    title = models.CharField(max_length=10, null=True, default=None)
 
     def __str__(self):
         return self.title
 
 
 class cuisine(models.Model):
-    title = models.CharField(max_length=200, null=True, default=None)
+    title = models.CharField(max_length=10, null=True, default=None)
 
     def __str__(self):
         return self.title
+
+
